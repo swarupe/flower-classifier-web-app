210c9da998229f455e39165e3846fd31e1d1f3c6
swarupe <swarup.e1998@gmail.com>
2019-01-11 17:29:25 +0530

    Fix to disable caching

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/', methods = ['GET', 'POST'])
 def predict():
     if request.method == 'GET':
